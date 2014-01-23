4ace6cfa58864e32409cf15d604b8f65568e7e06
Jim Rosser <jarosser06@gmail.com>
2014-01-22 20:23:31 -0600

    test new changes, use apt and yum for repo refresh

--- Berksfile
+++ Berksfile
@@ -2,5 +2,12 @@ site :opscode
 
 cookbook 'bluepill'
 cookbook 'apache2'
+cookbook 'java'
+cookbook 'elasticsearch'
+
+group :integration do
+  cookbook 'apt'
+  cookbook 'yum-epel'
+end
 
 metadata
--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
 depends          'bluepill'
 depends          'apache2'
+depends          'java'
+depends          'elasticsearch'
